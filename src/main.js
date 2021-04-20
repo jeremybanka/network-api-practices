1c19dcebbb90ede81a7738a25598399953a48477
Jeremy Banka <hello@jeremybanka.com>
2021-04-20 13:50:12 -0700

    🎨 rename some vars and funcs, specify some targets

--- src/main.js
+++ src/main.js
@@ -1,29 +1,34 @@
-import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
+import $ from "jquery"
+import "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./css/styles.css"
 
-$(document).ready(function() {
-  $('#weatherLocation').click(function() {
-    const city = $('#location').val();
-    $('#location').val("");
+$(document).ready(function () {
+  $("form#entry").on("submit", function (event) {
+    event.preventDefault()
+    const city = $("input#location").val()
+    $("input#location").val("")
 
-    let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+    let request = new XMLHttpRequest()
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`
 
-    request.onreadystatechange = function() {
+    request.onreadystatechange = function () {
       if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
+        const response = JSON.parse(this.responseText)
+        showElements(response)
       }
     }
 
-    request.open("GET", url, true);
-    request.send();
+    request.open("GET", url, true)
+    request.send()
 
-    function getElements(response) {
-      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+    function showElements(response) {
+      $("#showHumidity").text(
+        `The humidity in ${city} is ${response.main.humidity}%`
+      )
+      $("#showTemp").text(
+        `The temperature in Kelvins is ${response.main.temp} degrees.`
+      )
     }
-  });
-});
+  })
+})
